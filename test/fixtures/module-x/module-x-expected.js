be19c61f9c32377d41585ec0a419698e290b477a
Brendan Baldwin <brendan@usergenic.com>
2019-04-22 16:50:58 -0700

    WIP commit now we test for those scoped imports!

---
+++ test/fixtures/module-x/module-x-expected.js
@@ -0,0 +1,4 @@
+export { y } from "http://127.0.0.1:8081/module-x/module-y/module-y.js?koa-importmap=e82874a8dbe77ec6e4746a35f9d3065e2f2628913c30e1e785b73375baa98465";
+export function x() {
+  return 'ecks';
+}
--- test/rewriter-middleware-test.js
+++ test/rewriter-middleware-test.js
@@ -15,23 +15,29 @@ function makeServer() {
 }
 
 test('Middleware rewrites specifiers from inline importmap', async (t) => {
-  t.plan(1)
+  t.plan(2)
 
   const server = makeServer()
-  const response = await request(server).get('/index-a.html')
+  const indexResponse = await request(server).get('/index-a.html')
+  const moduleXResponse = await request(server).get('/module-x/module-x.js?koa-importmap=e82874a8dbe77ec6e4746a35f9d3065e2f2628913c30e1e785b73375baa98465')
   server.close()
-  t.deepEquals(simplifyHTML(response.text),
+  t.deepEquals(simplifyHTML(indexResponse.text),
     simplifyHTML(
-      fs.readFileSync(__dirname + '/fixtures/index-a-expected.html', 'utf8')))
+      fs.readFileSync(__dirname + '/fixtures/index-a-expected.html', 'utf8')),
+    'specifiers in inline modules rewritten')
+  t.deepEquals(simplifyHTML(moduleXResponse.text),
+    simplifyHTML(
+      fs.readFileSync(__dirname + '/fixtures/module-x/module-x-expected.js', 'utf8')),
+    'specifiers in external modules rewritten')
 })
 
 test('Middleware rewrites specifiers from external importmap', async (t) => {
   t.plan(1)
 
   const server = makeServer()
-  const response = await request(server).get('/index-b.html')
+  const indexResponse = await request(server).get('/index-b.html')
   server.close()
-  t.deepEquals(simplifyHTML(response.text),
+  t.deepEquals(simplifyHTML(indexResponse.text),
     simplifyHTML(
       fs.readFileSync(__dirname + '/fixtures/index-b-expected.html', 'utf8')))
 })
